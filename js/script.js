de82ff571854daa5311f7d907bd2dce6db480cb4
Oaken <oladelesamson37@yahoo.com>
2021-10-12 12:05:14 +0100

    added requests for new patient registration

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 // api call will be made here
 
-$( document ).ready(function() {
+$(document).ready(function() {
     let pages = ['js-dashboard'];
 
 
-    $('.js-dashboard, .nav-page').on('click', function(){
+    $('.js-dashboard, .nav-page').on('click', function() {
         $('.js-dashboard-page').removeClass('d-none');
         $('.js-dashboard').addClass('active');
         $('.js-review-visits-page').addClass('d-none');
         $('.js-review-visits').removeClass('active');
-        
+
     });
 
-    $('.js-review-visits').on('click', function(){
+    $('.js-review-visits').on('click', function() {
         $('.js-dashboard-page').addClass('d-none');
         $('.js-dashboard').removeClass('active');
         $('.js-review-visits-page').removeClass('d-none');
@@ -22,13 +22,70 @@ $( document ).ready(function() {
     });
 
 
+    $('.js-new-patient-btn').on('click', function() {
+        $('.visitation-page').addClass('d-none');
+        $('.new-patient-page').removeClass('d-none');
+    });
+
+    $('.js-generate-upi').on('click', function(e) {
+        e.preventDefault();
+        createNewPatient();
+    })
+
+
+    function setHash(hash) {
+        if (history.pushState) {
+            history.pushState(null, null, hash);
+        } else {
+            location.hash = hash;
+        }
+    }
+
+    var proxyurl = 'https://fathomless-garden-11176.herokuapp.com/';
     let baseUrl = 'https://chioma.herokuapp.com/v1';
 
     let data = {
-        
+
     }
 
-    function getReviewVisits(){
+    function getNewPatientData() {
+        extra = {
+            'sex': $('#sex').val(),
+            'marital_status': $('#maritalStatus').val(),
+            'blood_pressure': '80hp/120'
+        };
+
+        return {
+            "name": $('#fullname').val(),
+            "email": $('#email').val(),
+            "phone_number": $('#number').val(),
+            "password": "password123",
+            "user_type": "patient",
+            "extra_details": extra,
+            "hospital_id": "17ff72ef-1297-4d5d-99b8-133dd831f1cf"
+        };
+    }
+
+    function createNewPatient() {
+        $.ajax({
+            url: `${proxyurl + baseUrl}/auth/register`,
+            type: 'POST',
+            dataType: 'json', // added data type,
+            data: getNewPatientData(),
+
+            success: function(res) {
+                console.log(res);
+                alert(res);
+            },
+
+            error: function(res) {
+                console.log(res);
+                alert(res);
+            }
+        });
+    }
+
+    function getReviewVisits() {
         $.ajax({
             url: `${baseUrl}/visit`,
             type: 'POST',
